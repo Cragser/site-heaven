a8f568280ed4e286b9ccaaa26657dfcc65581b04
Sergio Reyes <sergio.reyes@plexusworldwide.com>
2024-07-07 15:46:01 -0600

    Disable lang es for vercel.

--- src/shared/providers/data-provider/index.ts
+++ src/shared/providers/data-provider/index.ts
@@ -5,6 +5,6 @@ import dataProviderSimpleRest from '@refinedev/simple-rest';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const API_URL = isDev ? 'http://localhost:3003/api': "https://api-heaven-production.up.railway.app/";
+const API_URL = isDev ? 'http://localhost:3003/api': "https://api-heaven-production.up.railway.app/api";
 
 export const dataProvider = dataProviderSimpleRest(API_URL);
