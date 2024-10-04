5b3c3bc9d4d659b1939c7d593692c3bb3e1cf03d
Cragser <cragser@hotmail.com>
2024-10-04 01:57:18 -0600

    Update dependencies and use million lint.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// export default MillionLint.next({
+//   rsc: true
+// })(nextConfig);
+
+// Solo aplica MillionLint si estás en modo desarrollo
+const withMillionLint =
+  process.env.NODE_ENV === "development"
+    ? MillionLint.next({ rsc: true })(nextConfig)
+    : nextConfig;
+
+export default withMillionLint;
