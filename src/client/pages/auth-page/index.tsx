5f58bbc26443ca53ecc5640c417646247dab57e7
Cragser <cragser@hotmail.com>
2024-12-04 03:06:51 -0600

    Update login

--- src/client/pages/auth-page/index.tsx
+++ src/client/pages/auth-page/index.tsx
@@ -1,15 +1,22 @@
-'use client';
-import { AuthPage as AuthPageBase } from '@refinedev/antd';
-import type { AuthPageProps } from '@refinedev/core';
+"use client";
+import { AuthPage as AuthPageBase } from "@refinedev/antd";
+import type { AuthPageProps } from "@refinedev/core";
 
 export const AuthPage = (props: AuthPageProps) => {
   return (
     <AuthPageBase
       {...props}
+      title={null}
+      renderContent={(content) => (
+        <div>
+          {/* Renderiza el contenido sin incluir un logo */}
+          {content}
+        </div>
+      )}
       formProps={{
         initialValues: {
-          email: 'demo@refine.dev',
-          password: 'demodemo',
+          email: "demo@formel.dev",
+          password: "formelpass",
         },
       }}
     />
