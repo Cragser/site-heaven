1c61ebd8faa2ef626c4207aaefa2ad969fc1b8a8
Cragser <cragser@hotmail.com>
2025-02-15 01:51:17 -0600

    Deploy multi repo vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,12 @@ import { Suspense } from "react";
 
 import { NavigateToResource } from "@refinedev/nextjs-router";
 import { Authenticated } from "@refinedev/core";
-import { Hello } from "@cragser/core-heaven";
 
 export default function IndexPage() {
-  const helloWorld: Hello = {
-    world: "Hello from core-heaven!",
-  };
   return (
     <Suspense fallback="loading">
       <Authenticated key="home-page" v3LegacyAuthProviderCompatible>
         <NavigateToResource />
-        <div>
-          <h1>Hello, {helloWorld.world}</h1>
-        </div>
       </Authenticated>
     </Suspense>
   );
